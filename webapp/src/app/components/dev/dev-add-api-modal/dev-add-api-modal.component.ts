781c12feb04fa6abe18b4bc49259adf5dedfdf62
NaikAayush <aayushnai@gmail.com>
2022-02-06 19:41:42 +0530

    api add modal fix

--- webapp/src/app/components/dev/dev-add-api-modal/dev-add-api-modal.component.ts
+++ webapp/src/app/components/dev/dev-add-api-modal/dev-add-api-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal/modal.service';
 
 @Component({
   selector: 'app-dev-add-api-modal',
@@ -9,12 +10,16 @@ export class DevAddApiModalComponent implements OnInit {
   loading: boolean = false;
   message: string = '';
   api_url: string = '';
-  constructor() {}
+  constructor(public modal: ModalService) {}
 
   ngOnInit(): void {}
 
   addAPI() {
     alert([this.message, this.api_url]);
     this.loading = true;
+  }
+
+  cancel() {
+    this.modal.addNewAPIModal = false;
   }
 }
---
+++ webapp/src/app/services/modal/modal.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  addNewAPIModal: boolean = false;
+  constructor() {}
+}
