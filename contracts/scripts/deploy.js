b50f9309ca9bec6acb2ec19dae5b2c5160cc8589
Samyak S Sarnayak <samyak201@gmail.com>
2022-02-06 18:01:51 +0530

    contract: update deploy script with superfluid init

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -2,7 +2,10 @@ async function main() {
    const PayMyAPI = await ethers.getContractFactory("PayMyAPI");
 
    // Start deployment, returning a promise that resolves to a contract object
-   const contract = await PayMyAPI.deploy();
+   // https://docs.superfluid.finance/superfluid/protocol-developers/networks
+   const host = "0xEB796bdb90fFA0f28255275e16936D25d3418603";
+   const supertoken = "0x42bb40bF79730451B11f6De1CbA222F17b87Afd7";
+   const contract = await PayMyAPI.deploy(host, supertoken);
    console.log("Contract deployed to address:", contract.address);
 }
 
