d039ebe548e05d12176e03122ad08e31d9b4216b
NaikAayush <aayushnai@gmail.com>
2022-02-06 15:04:57 +0530

    add verify message function to contract

--- contracts/contracts/PayMyAPI.sol
+++ contracts/contracts/PayMyAPI.sol
@@ -72,4 +72,18 @@ contract PayMyAPI {
     function unsubscribe(address developer) public {
         subscriptions[msg.sender][developer].active = false;
     }
+
+    using ECDSA for bytes32; 
+
+    function verifyMessage(string memory message, bytes memory signature) public view  returns(address, bool) {
+        bytes32 messagehash =  keccak256(bytes(message));
+       
+        address signeraddress = messagehash.toEthSignedMessageHash().recover(signature);
+              
+        if (msg.sender == signeraddress) {
+            return (signeraddress, true);
+        } else {
+            return (signeraddress, false);
+        }
+    }
 }
